9630cfac6d6533b8e1f30386fbe486f6efec523d
Bro Dev <me.ali.afif@gmail.com>
2026-07-25 23:13:23 +0100

    feat: added plant

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 "Coffee Mug (School Project)" (https://skfb.ly/6toBy) by Ole Gunnar Isager is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
 "Toasts" (https://skfb.ly/onZKp) by AK STUDIO is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+"[FREE] Pilea Peperomioides Terracotta pot" (https://skfb.ly/oDsOo) by AllQuad is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+"Watering Can [derivative]" (https://skfb.ly/pywqI) by romullus is licensed under Creative Commons Attribution-ShareAlike (http://creativecommons.org/licenses/by-sa/4.0/).
--- scenes/house/day1/day_1.gd
+++ scenes/house/day1/day_1.gd
@@ -6,7 +6,10 @@ enum STATE{
 	SHOWERING,
 	DONE_SHOWERING,
 	CURRENT_TASK_EAT_BREAKFAST,
-	DONE_EATING_BREAKFAST
+	DONE_EATING_BREAKFAST,
+	CURRENT_TASK_FIND_WATERING_CAN,
+	CURRENT_TASK_WATER_PLANT,
+	DONE_WATERING_PLANT
 }
 
 var state:STATE
@@ -30,6 +33,12 @@ func _process(delta: float) -> void:
 			done_showering()
 		STATE.CURRENT_TASK_EAT_BREAKFAST:
 			current_task_eat_breakfast(delta)
+		STATE.DONE_EATING_BREAKFAST:
+			done_eating_breakfast()
+		STATE.CURRENT_TASK_FIND_WATERING_CAN:
+			current_task_find_watering_can()
+		STATE.CURRENT_TASK_WATER_PLANT:
+			current_task_water_plant(delta)
 		_: pass
 
 var PlayerInsideShowerStateTrigger:bool = false
@@ -63,6 +72,38 @@ func _on_breakfast_eat_zone_body_exited(body: Node3D) -> void:
 		if state == STATE.CURRENT_TASK_EAT_BREAKFAST:
 			$UI.clear_tooltip()
 		PlayerInsideBreakfastEatZone = false
+
+var PlayerInsideWaterPlantTaskZone = false
+func _on_water_plant_task_zone_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		PlayerInsideWaterPlantTaskZone = true
+func _on_water_plant_task_zone_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		PlayerInsideWaterPlantTaskZone = false
+
+var PlayerInsideWateringCanPickupZone = false
+func _on_watering_can_pickup_zone_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		if state == STATE.CURRENT_TASK_FIND_WATERING_CAN:
+			$UI.tooltip("Press [ E ] to pickup.")
+		PlayerInsideWateringCanPickupZone = true
+func _on_watering_can_pickup_zone_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		if state == STATE.CURRENT_TASK_FIND_WATERING_CAN:
+			$UI.clear_tooltip()
+		PlayerInsideWateringCanPickupZone = false
+
+var PlayerInsideWaterPlantZone = false
+func _on_water_plant_zone_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		if state == STATE.CURRENT_TASK_WATER_PLANT:
+			$UI.tooltip("Hold [ E ] to water plant.")
+		PlayerInsideWaterPlantZone = true
+func _on_water_plant_zone_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		if state == STATE.CURRENT_TASK_WATER_PLANT:
+			$UI.clear_tooltip()
+		PlayerInsideWaterPlantZone = false
 
 
 var start_sequence_called = false
@@ -127,3 +168,36 @@ func current_task_eat_breakfast(delta:float)->void:
 		breakfast_progress = 0.0
 		$UI.hide_progress_bar()
 		$UI.set_progress(breakfast_progress)
+
+func done_eating_breakfast():
+	if PlayerInsideWaterPlantTaskZone:
+		state = STATE.CURRENT_TASK_FIND_WATERING_CAN
+		await $UI.say("That plant needs to be watered")
+		await get_tree().create_timer(2.0).timeout
+		$UI.task("> Find watering can")
+		$UI.clear_dialogue()
+
+func current_task_find_watering_can():
+	if PlayerInsideWateringCanPickupZone:
+		if Input.is_action_just_pressed("interact"):
+			$UI.task("> Go water plant")
+			$UI.clear_tooltip()
+			$Props/WateringCan.queue_free()
+			state = STATE.CURRENT_TASK_WATER_PLANT
+
+var watering_progress:float = 0.0
+const WATERING_SPEED:float = 10.0
+func current_task_water_plant(delta):
+	if PlayerInsideWaterPlantZone:
+		$UI.show_progress_bar()
+		$UI.set_progress(watering_progress)
+		if Input.is_action_pressed("interact"):
+			watering_progress += WATERING_SPEED * delta
+			watering_progress = clamp(watering_progress, 0.0, 100.0)
+		if watering_progress >= 100.0:
+			$UI.hide_progress_bar()
+			$UI.clear_task()
+			$UI.clear_tooltip()
+			state = STATE.DONE_WATERING_PLANT
+	else:
+		$UI.hide_progress_bar()
